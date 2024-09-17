fdced63a3a3a7b33fa1edd0e0f4479daec224025
kvdcre8r <kvdcre8r@gmail.com>
2024-09-17 13:16:15 -0500

    working on home page

--- src/Modules/Home/Home.jsx
+++ src/Modules/Home/Home.jsx
@@ -3,12 +3,19 @@ import "./home.css";
 
 export function Home() {
   return (
-    <div id="main-content">
     <main className="home">
-      <Header />
-      <section>
-        <h2>Welcome to the Live Resume of Web Developer Kevin VanDonkelaar! An interactive experience designed to showcase the talents and creativity of this aspiring Super-Devloper!</h2>
-      </section>
-    </main></div>
+      <div id="main-content">
+        <Header />
+        <section>
+          <article>
+            <h2>
+              Welcome to the Live Resume of Web Developer Kevin VanDonkelaar! An
+              interactive experience designed to showcase the talents and
+              creativity of this aspiring Super-Devloper!
+            </h2>
+          </article>
+        </section>
+      </div>
+    </main>
   );
 }
