fe70a6b7845dbe16bbea0f19a138835139cbf5f6
kvdcre8r <kvdcre8r@gmail.com>
2024-10-03 10:42:25 -0500

    completed responsiveness for homepage to 500px

--- src/Modules/Header/myLinks.jsx
+++ src/Modules/Header/myLinks.jsx
@@ -1,14 +1,14 @@
 export default function MyLinks() {
   return (
     <div className="my-links">
+      <a href="https://kevin-vandonkelaar-personal-website.github.io/kvdcre8r/">
+        Personal Website
+      </a>
       <a
         href="https://www.linkedin.com/in/kevinvandonkelaar"
         className="fa fa-linkedin"
       ></a>
       <a href="https://github.com/kvdcre8r" className="fa fa-github"></a>
-      <a href="https://kevin-vandonkelaar-personal-website.github.io/kvdcre8r/">
-        Personal Website
-      </a>
     </div>
   );
 }
